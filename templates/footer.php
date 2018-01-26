0664b215be751e46d7e8cc1a9cec73bafa1fd8ec
Arthur Beaucamp <arthur_beaucamp@laposte.net>
2018-01-26 14:08:47 +0100

    Base du projet

---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+    </main>
+    <footer></footer>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+  </body>
+</html>
