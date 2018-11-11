ccc5c57b2e4538d2735872854231477fdc922b98
Beaucamp Arthur <arthur_beaucamp@laposte.net>
2018-11-11 13:33:42 +0100

    Correction diverses

--- templates/actu.php
+++ templates/actu.php
@@ -2,10 +2,10 @@
   <h2>Nos Actualités</h2>
   <div id="containerNews">
     <h3>Lancement des inscriptions</h3>
-    <p>Les inscriptions seront disponible sur le site de <a href="http://www.sporkrono.fr/" target="_blank">
-    Sporkrono</a> à la date du 1er Décembre 2018 ! Attention, pour que l'inscription soit valide il faudra impérativement fournir le certificat médical lors de votre inscription sur internet. Certificat datant de <mark>moins d'un an</mark> et avec la mention <mark>"course à pied en compétition"</mark>. Attention ! Pas de remise de Certificat le jour de la course. Les places seront limitées à <mark>300 par course</mark> alors ne tardait pas.</p>
+    <p>Les inscriptions seront disponibles sur le site de <a href="http://www.sporkrono.fr/" target="_blank">
+    Sporkrono</a> à la date du 1er Décembre 2018 ! Attention, pour que l'inscription soit valide il faudra impérativement fournir le certificat médical lors de votre inscription sur internet. Certificat datant de <mark>moins d'un an</mark> et avec la mention <mark>"course à pied en compétition"</mark>. Attention ! Pas de remise de Certificat le jour de la course. Les places seront limitées à <mark>300 par course</mark> alors ne tardez pas.</p>
 
     <h3>Information</h3>
-    <p>Véhicules interdit sur le parcours <mark>y compris les motos</mark> merci de respecter les coureurs qui viennent ce retrouver en fôret et dans la nature sans les polluer. Veuillez noter que les vélos sont également <mark>interdit</mark> sur l'ensemble du parcours.</br>Course <mark>interdite au chien</mark>, autant sur la course que dans la salle. Merci pour votre compréhension.</p>
+    <p>Véhicules interdits sur le parcours <mark>y compris les motos</mark> merci de respecter les coureurs qui viennent se retrouver en fôret et dans la nature sans les polluer. Veuillez noter que les vélos sont également <mark>interdits</mark> sur l'ensemble du parcours.</br>Course <mark>interdite au chien</mark>, autant sur la course que dans la salle. Merci pour votre compréhension.</p>
   </div>
 </section>
